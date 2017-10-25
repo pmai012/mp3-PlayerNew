c3c43a84abb9a9bbe8ebb6a064c79bbddd80a9ec
Pascal <pascalmai@live.de>
2017-10-25 14:04:46 +0200

    add minim libraries

---
+++ src/Model/MP3Player.java
@@ -0,0 +1,21 @@
+package Model;
+
+import de.hsrm.mi.eibo.simpleplayer.SimpleAudioPlayer;
+import de.hsrm.mi.eibo.simpleplayer.SimpleMinim;
+
+/**
+ * Created by Pascal on 25.10.2017.
+ */
+public class MP3Player {
+    SimpleMinim minim;
+    SimpleAudioPlayer audioPlayer;
+
+    public MP3Player(){}
+
+    private void play(String fileName){}
+    private void play(){}
+    private void pause(){}
+    private void stop(){}
+    private void volume(float value){}
+    private void balance(float value){}
+}
