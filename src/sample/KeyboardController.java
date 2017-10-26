95b0ec3e089f16f18926af614278123737c7c364
kolone13 <30953267+kolone13@users.noreply.github.com>
2017-10-26 22:04:01 +0200

    Alle Befehl von Übung 1 sind theoretisch da, lassen sich jedoch nicht umsetzen, da man immer noch das Thread Problem hat. Es wurde die KeyboardController Klasse erstellt, da man die Controller Klasse für spätere Aufgaben gebrauchen könnte

--- src/sample/KeyboardController.java
+++ src/sample/KeyboardController.java
@@ -7,53 +7,66 @@ import Model.MP3Player;
  */
 public class KeyboardController {
 
-        public void befehl(String input) {
+    public void befehl(String input) {
+
+        MP3Player player = new MP3Player();
+        String[] command = input.split(" ");
 
 
+        for (int i = 0; i < command.length; i++) {
+            command[i] = command[i].toUpperCase();
 
 
-
-            MP3Player player = new MP3Player();
-            String[] command = input.split(" ");
-
+            //PLAY BEFEHL
+            if (command[i].equals("PLAY")) {
 
 
-            for (int i = 0; i < command.length; i++) {
-                command[i] = command[i].toUpperCase();
+                if (i + 1 == command.length) {
 
-
-                //PLAY BEFEHL
-                if (command[i].equals("PLAY")) {
-
-
-                    if (i + 1 == command.length) {
-
-                        player.play();
-                    } else {
-                        String title = "";
-                        for (int t = i + 1; t < command.length; t++) {
-                            title = title + command[t] + " ";
-                        }
-                        player.play(title);
+                    player.play();
+                } else {
+                    String title = "";
+                    for (int t = i + 1; t < command.length; t++) {
+                        title = title + command[t] + " ";
                     }
-
-
+                    player.play(title);
                 }
 
-                //PAUSE BEFEHL
-                if (command[i].equals("PAUSE")) {
-                    player.pause();
-                }
-
-                //PAUSE BEFEHL
-                if (command[i].equals("STOP")) {
-                    player.stop();
-                }
 
             }
+
+            //PAUSE BEFEHL
+            if (command[i].equals("PAUSE")) {
+                player.pause();
+            }
+
+            //PAUSE BEFEHL
+            if (command[i].equals("STOP")) {
+                player.stop();
+            }
+
+            if (command[i].equals("VOLUME")) {
+                if (i + 1 == command.length) {
+                    float value = Float.parseFloat(command[i + 1]);
+                    player.volume(value);
+                } else {
+                    System.out.println("Geben Sie bitte einen Wert mit!");
+                }
+            }
+
+            if (command[i].equals("BALANCE")) {
+                if (i + 1 == command.length) {
+                    float value = Float.parseFloat(command[i + 1]);
+                    player.balance(value);
+                } else {
+                    System.out.println("Geben Sie bitte einen Wert mit!");
+                }
+            }
+
         }
-
-
     }
+
+
+}
 
 
