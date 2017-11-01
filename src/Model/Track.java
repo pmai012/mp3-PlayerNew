5525503357f0ee37661820ebbd65234a42a675d0
kolone13 <30953267+kolone13@users.noreply.github.com>
2017-11-01 15:10:15 +0100

    Klasse Track wurde erstellt

--- src/Model/Track.java
+++ src/Model/Track.java
@@ -1,24 +1,66 @@
 package Model;
 
-    /**
+ import com.mpatric.mp3agic.*;
+
+ import java.io.IOException;
+
+/**
      * Created by Pascal on 29.10.2017.
      */
 
     public class Track {
-        private String name;
-        private String artist;
-        private String album;
-        private String path;
 
+    public String getTitle() {
+        return title;
+    }
 
-        public Track(String path) {
-           // Mp3File mp3file = new Mp3File("SomeMp3File.mp3");
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    private String title = null;
+        private String artist = null;
+        private String album = null;
+        private String genre = null;
+        private String comment = null;
+        private String path = null;
+        private String  year = null;
+        private Mp3File mp3file = null;
+
+    public Track(String path) throws InvalidDataException, IOException, UnsupportedTagException {
+        mp3file = new Mp3File(path);
+        if (mp3file.hasId3v1Tag()) {
+            ID3v1 id3tags = mp3file.getId3v1Tag();
+            title = id3tags.getTitle();
+            artist = id3tags.getArtist();
+            album = id3tags.getAlbum();
+            genre = id3tags.getGenreDescription();
+            comment = id3tags.getComment();
+            year = id3tags.getYear();
+
 
         }
 
-    public Track() {
-                this.path = null;
-
-            }
+    }
 
         }
