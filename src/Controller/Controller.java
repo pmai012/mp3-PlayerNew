4fe044955a41c3cf673217f6a65998ab7abbe325
kolone13 <30953267+kolone13@users.noreply.github.com>
2017-11-11 11:43:28 +0100

    Erste GUI ELemente, jedoch sehen sie scheiße aus

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,7 +1,13 @@
 package Controller;
 
+import Model.MP3Player;
+
+
 /**
  * Created by User on 06.11.2017.
  */
 public class Controller {
+
+
+
 }
--- src/sample/Frameapplication.java
+++ src/sample/Frameapplication.java
@@ -1,18 +1,23 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.layout.*;
-import javafx.stage.StageStyle;
+import javafx.scene.control.*;
+
 
 
 /**
  * Created by User on 06.11.2017.
  */
 public class Frameapplication extends Application {
+    final int WIDTH = 1200;
+    final int HEIGHT = 620;
+
 
     public void init() {
 
@@ -21,24 +26,37 @@ public class Frameapplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Button knopf = new Button("Klick mich ! ");
-
-
-        Pane root = new Pane();
-        root.getChildren().add(knopf);
-
+        //Fenstereinstellungen
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        primaryStage.setScene(scene);
+        // primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("3Player");
         root.setPadding(new Insets(20,0,0,0));
         root.setStyle("-fx-background-color: gold;");
 
-        Scene scene = new Scene(root, 1200, 620);
-        primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setTitle("3Player");
+        //BUTTON
+        Button play = new Button("Play ");
+        play.setLayoutX(WIDTH*0.5);
+        play.setLayoutY(HEIGHT*0.8 );
+
+        Label title = new Label("Title");
+        title.setLayoutX(WIDTH*0.5);
+        title.setLayoutY(HEIGHT*0.7);
+
+        Label album = new Label("Album");
+        album.setLayoutX(WIDTH*0.5);
+        album.setLayoutY(HEIGHT*0.73) ;
+
+
+
+
+
+
+        root.getChildren().add(album);
+        root.getChildren().add(title);
+        root.getChildren().add(play);
         primaryStage.show();
-
-
-
-
     }
 
     public void stop() {
